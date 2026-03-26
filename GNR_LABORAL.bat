404b113292b547da4d2ebbd0b770c5d89f323806
Vanessa Nascimento <vanessa.nascimento@tellescom.corp>
2026-03-26 07:04:56 -0400

    foi adiionado um bat para abrir com dois clicks na tela inicial

---
+++ GNR_LABORAL.bat
@@ -0,0 +1,7 @@
+@echo off
+title Sistema de Ginastica Laboral - IA
+echo Iniciando Reconhecimento Facial...
+:: Entra na pasta do ambiente virtual e roda o programa
+call .\venv\Scripts\activate
+python executar.py
+pause
